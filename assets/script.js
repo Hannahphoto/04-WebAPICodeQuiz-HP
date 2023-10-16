8096b16ffaadad755d1ba3b75a8f22969394ba94
Hannahphoto <hannahprice67@gmail.com>
2023-10-16 00:06:12 -0400

    first quiz question appearing on page. README update

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,48 @@
 var startquizBtn = document.querySelector("#startquiz");
 var timerEl = document.getElementById("timer");
 var clearEl = document.querySelector("#clear");
-var q1 = document.createElement("q1");   
+var questionEl = document.querySelector("#questiontitle");
 var q2 = document.createElement("q2");
-var q3 = document.createElement("q3");
-var q4 = document.createElement("q4");
-var q5 = document.createElement("q5");
-var q6 = document.createElement("q6");
-
-// document.querySelector('container').innerHTML = " ";
+const myQuestions = [
+    {
+        question: "Commonly used data types DO NOT include: ",
+        answers: ["#1 Strings", "#2 Booleans", "#3 Alerts", "#4 Numbers"],
+        correctAnswer: "#3 Alerts"
+    }
+    {
+        question: "The condition in an if/else statement is enclosed withing ____.",
+        answers: ["#1 Quotes", "#2 Curly Brackets", "#3 Parantheses", "#4 Square Brackets"],
+        correctAnswer: "#3 Paranthesis"
+    }
+    {
+        question: "Arrays in JavaScript can be used to store_________.",
+        answers: ["#1 Numbers & Strings", "#2 Other arrays", "#3 Booleans", "#4 All of the above"],
+        correctAnswer: "#4 All of the above"
+    }
+    {
+        question: "An object is surrounded by  ____.",
+        answers: ["#1 Quotes", "#2 Curly Brackets", "#3 Parantheses", "#4 Square Brackets"],
+        correctAnswer: "#2 Curly Brackets"
+    }
+    {
+        question: "An eventListener method is based off of ____.",
+        answers: ["#1 Site", "#2 Touch", "#3 Talking", "#4 Click"],
+        correctAnswer: "#4 Click"
+    }
+    {
+        question: "Arrays work best with what type of conditional statement?",
+        answers: ["#1 if/else", "#2 for", "#3 Let", "#4 When"],
+        correctAnswer: "#2 for"
+    }
+];
+// var q3 = document.createElement("q3");
+// var q4 = document.createElement("q4");
+// var q5 = document.createElement("q5");
+// var q6 = document.createElement("q6");
 
 // //WHEN I click the start button
 // THEN a timer starts and I am presented with a question
-function generateQuiz() {
+function runTimer() {
     var timeLeft = 60;
     var timeInterval = setInterval(function (){
     timeLeft--;
@@ -25,31 +55,24 @@ function generateQuiz() {
     // }
 }, 1000);
 }
-// if(generateQuiz === true){
-//     clearEl.innerHTML= " ";
-
-// }
 
 function clear (){
     clearEl.document.innerHTML = " ";
 }
 
 function quiz (){
-    q1.textContent = ["Commonly used data types DO NOT include: ", "#1 Strings", "#2 Booleans", "#3 Alerts", "#4 Numbers"];
-    q2.text
+    var q = document.createElement("p");   
+    q.textContent = myQuestions[0].question;
+    questionEl.appendChild(q);
 }
 
 function startquiz(){
-    quiz = generateQuiz(); 
+    runTimer();
+    quiz(); 
    
 }
 
-
-
-startquizBtn.addEventListener("click", startquiz)
-;
-// startquizBtn.addEventListener("click", clear);
-
+startquizBtn.addEventListener("click", startquiz);
 
 
 // WHEN I answer a question
